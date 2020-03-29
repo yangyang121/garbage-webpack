3a8e8199f253767d96d291f3b3b076424ca0ad9f
yangyang121 <844171995@qq.com>
2020-03-29 12:02:56 +0800

    finish request

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,6 +1,73 @@
 import React from "react";
+import { limitRequest, IRequest } from "../utils";
 
 function Home() {
+  const requestArr: IRequest[] = [
+    {
+      url: "/one",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/two",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/three",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/four",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/five",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/six",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/seven",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/eight",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/nine",
+      cb: data => {
+        console.log(data);
+      }
+    },
+    {
+      url: "/ten",
+      cb: data => {
+        console.log(data);
+      }
+    }
+  ];
+  const complete = () => {
+    console.log("complete");
+  };
+  limitRequest(5, requestArr, complete);
   return <div>Home</div>;
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+export interface IRequest {
+  url: string;
+  cb?: (data: any) => void;
+}
+
 export const shuffleKeyboard = () => {
   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let i = arr.length - 1;
@@ -7,4 +12,35 @@ export const shuffleKeyboard = () => {
     i--;
   }
   return arr;
+};
+
+const mockRequest = (url: string) => {
+  console.log(url);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ response: url });
+    }, Math.floor(Math.random() * 10) * 1000);
+  });
+};
+
+export const limitRequest = (
+  limit: number,
+  requestArr: IRequest[],
+  completeFn?: Function
+) => {
+  let index = 0;
+  let completeIndex = 0;
+  const handleRequest = (data: IRequest) => {
+    mockRequest(data.url).then(res => {
+      completeIndex++;
+      data.cb && data.cb(res);
+      index < requestArr.length && handleRequest(requestArr[index++]);
+      if (completeIndex === requestArr.length && completeFn) {
+        completeFn();
+      }
+    });
+  };
+  while (index < limit) {
+    handleRequest(requestArr[index++]);
+  }
 };
