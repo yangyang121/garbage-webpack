31e5c7eb2b3b5606cc0369915e5d6afd65f42d49
yangyang121 <844171995@qq.com>
2020-12-05 20:54:48 +0800

    update

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from "react"
+import { topKMaxHeap } from "../utils"
+
+const arr: number[] = [3, 4, 1, 7, 5, 9, 13, 2, -3]
+topKMaxHeap(arr, 3)
 
 function Home() {
-  return <div>Home</div>;
+  return <div>Home</div>
 }
 
-export default Home;
+export default Home
--- src/utils.ts
+++ src/utils.ts
@@ -1,76 +1,77 @@
 export interface IRequest {
-  url: string;
-  cb?: (data: any) => void;
+  url: string
+  cb?: (data: any) => void
 }
 
+// 洗牌算法
 export const shuffleKeyboard = () => {
-  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let i = arr.length - 1;
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let i = arr.length - 1
   while (i > 0) {
-    const changeNum = Math.floor(Math.random() * (i - 1));
-    [arr[i], arr[changeNum]] = [arr[changeNum], arr[i]];
-    i--;
+    const changeNum = Math.floor(Math.random() * (i - 1))
+    ;[arr[i], arr[changeNum]] = [arr[changeNum], arr[i]]
+    i--
   }
-  return arr;
-};
+  return arr
+}
 
 const mockRequest = (url: string) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve({ response: url });
-    }, Math.floor(Math.random() * 10) * 1000);
-  });
+      resolve({ response: url })
+    }, Math.floor(Math.random() * 10) * 1000)
+  })
 
 export const limitRequest = (
   limit: number,
   requestArr: IRequest[],
   completeFn?: Function
 ) => {
-  let index = 0;
-  let completeIndex = 0;
+  let index = 0
+  let completeIndex = 0
   const handleRequest = (data: IRequest) => {
     mockRequest(data.url).then((res) => {
-      completeIndex++;
-      data.cb && data.cb(res);
-      index < requestArr.length && handleRequest(requestArr[index++]);
+      completeIndex++
+      data.cb && data.cb(res)
+      index < requestArr.length && handleRequest(requestArr[index++])
       if (completeIndex === requestArr.length && completeFn) {
-        completeFn();
+        completeFn()
       }
-    });
-  };
-  while (index < Math.min(limit, requestArr.length)) {
-    handleRequest(requestArr[index++]);
+    })
   }
-};
+  while (index < Math.min(limit, requestArr.length)) {
+    handleRequest(requestArr[index++])
+  }
+}
 
 type UniversalObject = {
-  [propname: string]: string;
-};
+  [propname: string]: string
+}
 
 export const printf = (str: string, obj: UniversalObject) => {
-  let result = str;
-  let startIndex = result.indexOf("${");
-  let endIndex = result.indexOf("}");
+  let result = str
+  let startIndex = result.indexOf("${")
+  let endIndex = result.indexOf("}")
   while (startIndex > -1 && endIndex > -1) {
     result =
       result.slice(0, startIndex) +
       obj[result.slice(startIndex + 2, endIndex)] +
-      result.slice(endIndex + 1);
-    startIndex = result.indexOf("${");
-    endIndex = result.indexOf("}");
+      result.slice(endIndex + 1)
+    startIndex = result.indexOf("${")
+    endIndex = result.indexOf("}")
   }
 
-  console.log(result);
-};
+  console.log(result)
+}
 
-const numToWord = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
-const numToQuantile = ["", "十", "百", "千"];
-const numToBigQuantile = ["", "万", "亿"];
+const numToWord = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
+const numToQuantile = ["", "十", "百", "千"]
+const numToBigQuantile = ["", "万", "亿"]
 
 export const convertToRMB = (count: number) => {
-  const str = String(count);
-  let result = "";
-  const len = str.length;
+  const str = String(count)
+  let result = ""
+  const len = str.length
 
   for (let i = 0; i < len; i++) {
     result +=
@@ -78,39 +79,213 @@ export const convertToRMB = (count: number) => {
         ? result[result.length - 1] === "零"
           ? ""
           : "零"
-        : numToWord[Number(str[i])] + numToQuantile[(len - i - 1) % 4];
+        : numToWord[Number(str[i])] + numToQuantile[(len - i - 1) % 4]
     if ((len - i - 1) % 4 === 0) {
-      if (result[result.length - 1] === "零") result = result.slice(0, -1);
-      result += numToBigQuantile[(len - i - 1) / 4];
+      if (result[result.length - 1] === "零") result = result.slice(0, -1)
+      result += numToBigQuantile[(len - i - 1) / 4]
     }
   }
 
   if (count % 100 === 0) {
-    result += "元整";
+    result += "元整"
   } else {
-    result += "元";
+    result += "元"
   }
 
-  console.log(result);
-  return result;
-};
+  console.log(result)
+  return result
+}
 
 export const summaryRanges = (arr: number[]) => {
-  if (arr.length <= 1) return arr;
-  let start = 0;
-  let end = 0;
-  let temp = 0;
-  const result: string[] = [];
+  if (arr.length <= 1) return arr
+  let start = 0
+  let end = 0
+  let temp = 0
+  const result: string[] = []
   while (end < arr.length) {
-    temp = arr[end++];
+    temp = arr[end++]
     if (arr[end] - temp !== 1) {
       if (end - start === 1) {
-        result.push(arr[start].toString());
+        result.push(arr[start].toString())
       } else {
-        result.push(`${arr[start]}->${temp}`);
+        result.push(`${arr[start]}->${temp}`)
       }
-      start = end;
+      start = end
     }
   }
-  return result;
-};
+  return result
+}
+
+// topK 的两种解法，即堆和快排思想
+class MinHeap {
+  private container: number[]
+  constructor(arr: number[]) {
+    this.container = []
+    arr.forEach((item) => this.insert(item))
+  }
+
+  insert(data: number) {
+    const { container } = this
+    container.push(data)
+    let index = container.length - 1
+    let parent = (index - 1) >> 1
+    while (index) {
+      if (container[index] >= container[parent]) break
+      ;[container[index], container[parent]] = [
+        container[parent],
+        container[index],
+      ]
+      index = parent
+      parent = (index - 1) >> 1
+    }
+  }
+
+  extract() {
+    const { container } = this
+    if (!container.length) return null
+    ;[container[0], container[container.length - 1]] = [
+      container[container.length - 1],
+      container[0],
+    ]
+    const ans = container.pop()
+    let index = 0
+    let exchange = index * 2 + 1
+    while (exchange < container.length) {
+      let right = index * 2 + 2
+      if (right < container.length && container[right] <= container[exchange]) {
+        exchange = right
+      }
+      if (container[exchange] > container[index]) break
+      ;[container[index], container[exchange]] = [
+        container[exchange],
+        container[index],
+      ]
+      index = exchange
+      exchange = index * 2 + 1
+    }
+    return ans
+  }
+
+  top() {
+    if (this.container.length) return this.container[0]
+    return null
+  }
+
+  getContainer() {
+    return this.container
+  }
+}
+
+class MaxHeap {
+  private container: number[]
+  constructor(arr: number[]) {
+    this.container = []
+    arr.forEach((item) => this.insert(item))
+  }
+
+  insert(data: number) {
+    const { container } = this
+    container.push(data)
+    let index = container.length - 1
+    let parent = (index - 1) >> 1
+    while (index) {
+      if (container[index] <= container[parent]) break
+      ;[container[index], container[parent]] = [
+        container[parent],
+        container[index],
+      ]
+      index = parent
+      parent = (index - 1) >> 1
+    }
+  }
+
+  extract() {
+    const { container } = this
+    if (!container.length) return null
+    ;[container[0], container[container.length - 1]] = [
+      container[container.length - 1],
+      container[0],
+    ]
+    const ans = container.pop()
+    let index = 0
+    let exchange = index * 2 + 1
+    while (exchange < container.length) {
+      let right = index * 2 + 2
+      if (right < container.length && container[right] > container[exchange]) {
+        exchange = right
+      }
+      if (container[exchange] <= container[index]) break
+      ;[container[index], container[exchange]] = [
+        container[exchange],
+        container[index],
+      ]
+      index = exchange
+      exchange = index * 2 + 1
+    }
+    return ans
+  }
+
+  top() {
+    if (this.container.length) return this.container[0]
+    return null
+  }
+
+  getContainer() {
+    return this.container
+  }
+}
+
+export const topKMaxHeap = (arr: number[], k: number, isSmall = true) => {
+  const length = arr.length
+  if (k >= length) {
+    return arr
+  }
+
+  const heap = isSmall
+    ? new MaxHeap(arr.slice(0, k))
+    : new MinHeap(arr.slice(0, k))
+  for (let i = k; i < length; i++) {
+    const top = heap.top()
+    if (top && (isSmall ? top > arr[i] : top < arr[i])) {
+      heap.extract()
+      heap.insert(arr[i])
+    }
+  }
+  console.log(heap.getContainer())
+  return heap.getContainer()
+}
+
+function partition(arr: number[], left: number, right: number): number {
+  const x = arr[left]
+  let i = left
+  let j = right
+  while (i < j) {
+    while (i < j && arr[j] > x) {
+      j--
+    }
+    if (i < j) arr[i] = arr[j]
+    while (i < j && arr[i] < x) {
+      i++
+    }
+    if (i < j) arr[j] = arr[i]
+  }
+  arr[i] = x
+
+  return i
+}
+
+export const topKQuickSort = (arr: number[], k: number) => {
+  let left = 0
+  let right = arr.length - 1
+  let index = partition(arr, left, right)
+  while (index !== k) {
+    if (index < k) {
+      left = index + 1
+    } else if (index > k) {
+      right = index - 1
+    }
+    index = partition(arr, left, right)
+  }
+  console.log(arr)
+  return arr.slice(0, k)
+}
